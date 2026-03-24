c67d558b4140d8b4ae9ac7ee9cad5dc1bba2736a
pashathelooser <pavelvatutin08@gmail.com>
2026-03-24 16:15:37 +0500

    5_done

--- express-app/routes/users.js
+++ express-app/routes/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+let curId = 2;
 const users = {
   items: [
     { id: 1, name: "Лиза" },
@@ -10,6 +11,15 @@ const users = {
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send(users.items.map(item => item.name));
+});
+
+router.post('/', function(req, res, next) {
+  const {name} = req.body;
+  const newUser = {id: curId++, name: name}
+  users.items.push(newUser);
+  
+  res.status(201).json(newUser);
+  res.send(newUser);
 });
 
 module.exports = router;
